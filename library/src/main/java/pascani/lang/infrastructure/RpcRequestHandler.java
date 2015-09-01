5a46edb5ec7b892c41c406163aa8e4f9a3a61908
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-08-31 19:41:32 -0500

    :sparkles: Create an interface for RPC requests handler

--- library/src/main/java/pascani/lang/infrastructure/RpcRequestHandler.java
+++ library/src/main/java/pascani/lang/infrastructure/RpcRequestHandler.java
@@ -2,8 +2,19 @@ package pascani.lang.infrastructure;
 
 import java.io.Serializable;
 
+/**
+ * TODO: documentation
+ * 
+ * @author Miguel Jiménez - Initial contribution and API
+ */
 public interface RpcRequestHandler {
 
+	/**
+	 * TODO: documentation
+	 * 
+	 * @param request
+	 * @return
+	 */
 	public Serializable handle(RpcRequest request);
 	
 }
