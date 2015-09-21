a6982ff27df21f8ddf0d10a9febe4fb90f9ef687
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-09-21 10:25:29 -0500

    :zap: Update the Quartz wrapper implementation
    This update:
    
    - Adds a method to schedule jobs using a data map to provide context
    data
    - Adds a method to shutdown the scheduler

--- library/src/main/java/pascani/lang/util/EventScheduler.java
+++ library/src/main/java/pascani/lang/util/EventScheduler.java
@@ -6,6 +6,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 import org.quartz.CronExpression;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -62,6 +63,27 @@ public class EventScheduler {
 	 *             there is an internal Scheduler error.
 	 */
 	public static void schedule(Class<? extends Job> jobClass,
+			CronExpression expression, JobDataMap data)
+			throws SchedulerException {
+
+		JobDetail jobDetail = newJob(jobClass).usingJobData(data).build();
+		Trigger trigger = newTrigger().startNow()
+				.withSchedule(cronSchedule(expression)).build();
+
+		schedule(jobDetail, trigger);
+	}
+
+	/**
+	 * Creates an instance of {@link JobDetail} using the given {@link Job}
+	 * class, and an instance of {@link Trigger} from the {@link CronExpression}
+	 * object. Then, add the <code>JobDetail</code> to the scheduler, and
+	 * associate the <code>Trigger</code> with it.
+	 * 
+	 * @throws SchedulerException
+	 *             if the Job or Trigger cannot be added to the Scheduler, or
+	 *             there is an internal Scheduler error.
+	 */
+	public static void schedule(Class<? extends Job> jobClass,
 			CronExpression expression) throws SchedulerException {
 
 		JobDetail jobDetail = newJob(jobClass).build();
@@ -69,6 +91,21 @@ public class EventScheduler {
 				.withSchedule(cronSchedule(expression)).build();
 
 		schedule(jobDetail, trigger);
+	}
+	
+	/**
+	 * Halts the <code>Scheduler</code>'s firing of <code>{@link Trigger}s</code>,
+     * and cleans up all resources associated with the Scheduler. Equivalent to
+     * <code>shutdown(false)</code>.
+     * 
+     * <p>
+     * The scheduler cannot be re-started.
+     * </p>
+     * 
+     * @see Scheduler#shutdown(boolean)
+	 */
+	public static void shutdown() throws SchedulerException {
+		scheduler.shutdown();
 	}
 
 }
