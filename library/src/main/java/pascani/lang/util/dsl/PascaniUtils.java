692cad3fe391b282c46ba161b739a00ad0e6e103
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-01-09 00:53:07 -0500

    Remove the ServiceManager class and move its methods to PascaniUtils

--- library/src/main/java/pascani/lang/util/dsl/PascaniUtils.java
+++ library/src/main/java/pascani/lang/util/dsl/PascaniUtils.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright © 2015 Universidad Icesi
+ * 
+ * This file is part of the Pascani library.
+ * 
+ * The Pascani library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * The Pascani library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Pascani library. If not, see <http://www.gnu.org/licenses/>.
+ */
 package pascani.lang.util.dsl;
+
+import java.util.Map;
 
 import pascani.lang.events.ExceptionEvent;
 import pascani.lang.events.InvokeEvent;
@@ -38,6 +58,24 @@ public class PascaniUtils {
 	 */
 	public static ProbeProxy newPerformanceProbe(String uniqueName) {
 		return createProbeProxy(uniqueName);
+	}
+
+	/**
+	 * Registers the necessary properties to bind a SCA service
+	 * 
+	 * @param properties
+	 *            A map containing the necessary data to bind the service
+	 * @param clazz
+	 *            The service interface
+	 * @return null
+	 */
+	public static <T> T bindService(Map<String, Object> properties,
+			Class<T> clazz) {
+		/*
+		 * At runtime, inside Pascani, it is not important to have the actual
+		 * instance.
+		 */
+		return null;
 	}
 	
 	private static ProbeProxy createProbeProxy(String routingKey) {
