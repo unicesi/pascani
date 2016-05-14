645b5b69cb8bd26a5df7b3bad0390485948efef6
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-05-14 14:34:32 -0500

    Rename source folder

--- web/dashboard/webpack.production.config.js
+++ web/dashboard/webpack.production.config.js
@@ -8,7 +8,7 @@ var StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
     entry: [
-        path.join(__dirname, 'app/main.js')
+        path.join(__dirname, 'src/main.js')
     ],
     output: {
         path: path.join(__dirname, '/dist/'),
@@ -18,7 +18,7 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new HtmlWebpackPlugin({
-            template: 'app/index.tpl.html',
+            template: 'src/index.tpl.html',
             inject: 'body',
             filename: 'index.html'
         }),
