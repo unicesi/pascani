58bbb9846bb16dcf33d31e7455547ca736ad017d
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-05-15 16:50:35 -0500

    Introduce react panels

---
+++ web/dashboard/src/layout/ReactPanel.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+class ReactPanel extends Component {
+
+	static defaultProps = {
+		active: false,
+		floating: false,
+		width: 'regular',
+		id: undefined,
+		onBeforeOpen: (panel) => {},
+		onBeforeClose: (panel) => {},
+		claimActiveState: (panel) => {},
+		closePanel: (panel) => {},
+	}
+
+	static propTypes = {
+		active: React.PropTypes.bool,
+		floating: React.PropTypes.bool,
+		width: React.PropTypes.oneOfType([
+			React.PropTypes.string,
+			React.PropTypes.number,
+		]),
+		id: React.PropTypes.string,
+		onBeforeOpen: React.PropTypes.func,
+		onBeforeClose: React.PropTypes.func,
+		claimActiveState: React.PropTypes.func,
+		closePanel: React.PropTypes.func,
+	}
+
+	state = {
+		expanded: false,
+	}
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentWillMount() {
+		this.props.onBeforeOpen(this);
+	}
+
+	componentWillUnmount() {
+		this.props.onBeforeClose(this);
+	}
+
+	handleClose = (e) => {
+		e.preventDefault();
+		this.props.closePanel(this);
+	}
+
+	handleExpand = (e) => {
+		e.preventDefault();
+		this.setState({ expanded: true });
+		this.props.claimActiveState(this);
+	}
+
+	handleCollapse = (e) => {
+		e.preventDefault();
+		this.setState({ expanded: false });
+		this.props.claimActiveState(this);
+	}
+
+	render() {
+		const buttons = this.bindEventHandlers();
+		const classes = `${this.props.width}` 
+			+ (this.props.floating ? " floating" : "")
+			+ (this.props.active ? " active" : "")
+			+ (this.state.expanded ? " expanded" : "");
+		return (
+			<section id={this.props.id} className={"panel " + classes}>
+				{buttons}
+				{this.props.children}
+			</section>
+		);
+	}
+
+	bindEventHandlers = () => {
+		const buttons = [];
+		if (this.props.closeBtn) {
+			buttons.push(React.cloneElement(this.props.closeBtn, {
+				key: 'closeBtn',
+				onClick: this.handleClose
+			}));
+		}
+		if (this.props.expandBtn) {
+			buttons.push(React.cloneElement(this.props.expandBtn, {
+				key: 'expandBtn',
+				onClick: this.handleExpand
+			}));
+		}
+		if (this.props.collapseBtn) {
+			buttons.push(React.cloneElement(this.props.collapseBtn, {
+				key: 'collapseBtn',
+				onClick: this.handleCollapse
+			}));
+		}
+		return buttons;
+	}
+
+}
+
+export default ReactPanel;
---
+++ web/dashboard/src/layout/ReactPanels.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import ReactPanel from './ReactPanel';
+import update from 'react-addons-update';
+import isArray from 'isarray'
+
+class ReactPanels extends Component {
+
+	state = {
+		activePanel: undefined,
+		panels: [],
+	}
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		if(isArray(this.props.children)) {
+			this.props.children.map(this.push);
+		} else {
+			this.push(this.props.children);
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		this.ensureActivePanelVisible();
+	}
+
+	ensureActivePanelVisible = () => {
+		if (this.state.activePanel) {
+			const domNode = ReactDOM.findDOMNode(this.refs["panel-active"]);
+			domNode.scrollIntoView({
+				block: "end",
+				behavior: "smooth",
+			});
+		}
+	}
+
+	renderPanel = (panel, i) => {
+		const active = this.state.activePanel.props.id === panel.props.id;
+		const key = active ? "active" : panel.props.id;
+		return React.cloneElement(panel, {
+			key: key,
+			ref: `panel-${key}`,
+			active: active,
+			closePanel: this.remove,
+			claimActiveState: this.updateActivePanel
+		});
+	}
+
+	updateActivePanel = (panel) => {
+		this.setState({ activePanel: panel });
+	}
+
+	push = (panel) => {
+		const _panel = panel.props.id ? panel : React.cloneElement(panel, {
+			id: this.randomId(6)
+		});
+		this.setState({
+			activePanel: _panel,
+			panels: this.state.panels.concat([_panel])
+		});
+	}
+
+	remove = (panel) => {
+		const index = this.state.panels.map((p) => { 
+			return p.props.id 
+		}).indexOf(panel.props.id);
+		if (index > -1) {
+			this.setState({
+				panels: update(this.state.panels, {$splice: [[index, 1]]})
+			});
+			if (panel.props.active) {
+				const index2 = index > 0 ? index - 1 : index + 1;
+				this.setState({ activePanel: this.state.panels[index2] });
+			}
+		}
+	}
+
+	render() {
+		return (
+			<div className="panels">
+				{this.state.panels.map(this.renderPanel)}
+			</div>
+		);
+	}
+
+	// Source: http://stackoverflow.com/a/6861381/738968
+	randomId = (length) => {
+		const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+		let str = '';
+		if (!length) {
+			length = Math.floor(Math.random() * chars.length);
+		}
+		for (var i = 0; i < length; i++) {
+			str += chars[Math.floor(Math.random() * chars.length)];
+		}
+		return str;
+	}
+}
+
+export default ReactPanels;
