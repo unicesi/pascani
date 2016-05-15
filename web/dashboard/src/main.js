f042f0d3d062fcaa3f366b57f2c4fd4282d7f957
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-05-15 18:19:18 -0500

    Open only 1 db connection at the entry script

--- web/dashboard/src/main.js
+++ web/dashboard/src/main.js
@@ -1,7 +1,18 @@
-import 'jquery';
-import 'metro';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRethinkdb from 'react-rethinkdb';
+
+import 'jquery';
+import 'metro';
 import App from './App';
+
+// Open a react-rethinkdb session (a WebSocket connection to the server)
+ReactRethinkdb.DefaultSession.connect({
+	host: 'localhost', // hostname of the websocket server
+	port: 3000,        // port number of the websocket server
+	path: '/db',       // HTTP path to websocket route
+	secure: false,     // set true to use secure TLS websockets
+	db: 'test',        // default database, passed to rethinkdb.connect
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- web/dashboard/src/panels/Turtles.js
+++ web/dashboard/src/panels/Turtles.js
@@ -4,15 +4,6 @@ import reactMixin from 'react-mixin';
 
 const r = ReactRethinkdb.r;
 
-// Open a react-rethinkdb session (a WebSocket connection to the server)
-ReactRethinkdb.DefaultSession.connect({
-	host: 'localhost', // hostname of the websocket server
-	port: 3000,        // port number of the websocket server
-	path: '/db',       // HTTP path to websocket route
-	secure: false,     // set true to use secure TLS websockets
-	db: 'test',        // default database, passed to rethinkdb.connect
-});
-
 class Turtles extends Component {
 
 	constructor(props) {
